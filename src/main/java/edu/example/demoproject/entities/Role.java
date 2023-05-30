8dbdf6f1d6f95bd3de06b5f61de3b1724450eb2a
Begayym <begayymm@gmail.com>
2023-05-30 21:17:37 +0200

    added two initial entities

---
+++ src/main/java/edu/example/demoproject/entities/Role.java
@@ -0,0 +1,31 @@
+package edu.example.demoproject.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @Column(name = "role_name")
+    private String role;
+
+    @ManyToMany
+    @JoinTable(name = "users_roles",
+            joinColumns = {@JoinColumn(name = "roles", referencedColumnName = "role_id")},
+            inverseJoinColumns = {@JoinColumn(name = "users", referencedColumnName = "user_id")})
+    private List<User> userList;
+}
