e648796c59a801fef49f95f8d12d198da0530989
Begayym <begayymm@gmail.com>
2023-06-02 15:14:09 +0200

    can not get to be seen my logo.png

--- src/main/java/edu/example/demoproject/config/PathResourceConfiguration.java
+++ src/main/java/edu/example/demoproject/config/PathResourceConfiguration.java
@@ -1,11 +1,19 @@
 package edu.example.demoproject.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class PathResourceConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/main-page").setViewName("main-page");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/files/**")
